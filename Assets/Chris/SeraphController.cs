628043f0ca75cd2ec691df1c554dbaa067d0bac7
cstamford <cstamford@gmail.com>
2014-02-26 15:30:35 +0000

    framework for seraph controller revamp

--- Assets/Chris/SeraphController.cs
+++ Assets/Chris/SeraphController.cs
@@ -1,16 +1,49 @@
 // Controller for the main character - a Seraph.
 // Handles gliding, flight, and ground movement mechanics.
-// Requires a box collider and non-kinematic rigid body for collisions.
+//
+// Requires a box collider (set to trigger) and kinematic rigid body 
+// set on the object with this script for collisions.
+//
+// The rigid body needs constraints on all axis to be active.
 
 using UnityEngine;
 
 namespace cst
 {
-    public class SeraphController : MonoBehaviour
+    public interface IControllerType
     {
-        // Inspector fields
-        [SerializeField] private float RESTING_SPEED = 100.0f;
-        [SerializeField] private float MAX_SPEED = 250.0f;
+        void update();
+        void triggerEnter(Collider other);
+        void collisionEnter(Collision other);
+    }
+
+    public class GroundController : IControllerType
+    {
+        private readonly SeraphController m_parent;
+
+        public GroundController(SeraphController parent)
+        {
+            m_parent = parent;
+        }
+
+        public void update()
+        {
+        }
+
+        public void triggerEnter(Collider other)
+        {
+            Debug.Log(GetType().Name + " triggerEnter()");
+        }
+
+        public void collisionEnter(Collision other)
+        {
+            Debug.Log(GetType().Name + " collisionEnter()");
+        }
+    }
+
+    public class GlideController : IControllerType
+    {
+        private readonly SeraphController m_parent;
 
         private const float MAX_ROLL_ANGLE = 37.5f;
         private const float MAX_PITCH_ANGLE = 65.0f;
@@ -19,41 +52,39 @@ namespace cst
         private const float RETURN_TURN_SPEED = 30.0f;
         private const float INCREMENT_PITCH_SPEED = 45.0f;
         private const float INCREMENT_FOWARD_SPEED = 15.0f;
+        private const float RESTING_SPEED = 100.0f;
+        private const float MAX_SPEED = 250.0f;
 
         private Vector3 m_position;
         private Vector3 m_rotation;
+
         private float m_forwardSpeed;
 
-        // Initialise the flight variables
-        public void Start()
+        public GlideController(SeraphController parent)
         {
-            m_position = transform.position;
-            m_rotation = transform.eulerAngles;
+            m_parent = parent;
             m_forwardSpeed = RESTING_SPEED;
         }
 
-        // Update the position
-        public void Update()
+        public void update()
         {
-            float delta = Time.deltaTime;
-            handleOrientationChange(delta);
-            handleMoveForward(delta);
+            m_rotation = m_parent.getTransform().eulerAngles;
+            m_position = m_parent.getTransform().position;
+
+            handleOrientationChange(Time.deltaTime);
+            handleMoveForward(Time.deltaTime);
         }
 
-        public void OnCollisionEnter(Collision other)
+        public void triggerEnter(Collider other)
         {
-            Debug.Log("Collision ENTER!");
+            Debug.Log(GetType().Name + " triggerEnter()");
         }
 
-        // Emergency!
-        public void OnCollisionStay(Collision other)
+        public void collisionEnter(Collision other)
         {
-            Debug.Log("Collision STAY!");
-        }
-
-        public void OnTriggerEnter(Collider other)
-        {
-            Debug.Log("Trigger ENTER!");
+            Debug.Log(GetType().Name + " collisionEnter()");
+            m_parent.getTransform().position -= m_parent.getTransform().forward
+                * m_forwardSpeed * Time.deltaTime * 2.0f;
         }
 
         private void handleOrientationChange(float delta)
@@ -62,7 +93,7 @@ namespace cst
             handleYawChange(delta);
             handleRollChange(delta);
 
-            transform.eulerAngles = m_rotation;
+            m_parent.getTransform().eulerAngles = m_rotation;
         }
 
         // Handles the change in pitch based on the user input
@@ -133,7 +164,7 @@ namespace cst
             if (angle > -MAX_PITCH_ANGLE)
             {
                 m_rotation.x = Helpers.wrapAngle(
-                    m_rotation.x - (delta*INCREMENT_PITCH_SPEED));
+                    m_rotation.x - (delta * INCREMENT_PITCH_SPEED));
             }
         }
 
@@ -145,7 +176,7 @@ namespace cst
             if (angle < MAX_PITCH_ANGLE)
             {
                 m_rotation.x = Helpers.wrapAngle(
-                    m_rotation.x + (delta*INCREMENT_PITCH_SPEED));
+                    m_rotation.x + (delta * INCREMENT_PITCH_SPEED));
             }
         }
 
@@ -163,7 +194,7 @@ namespace cst
             if (angle < MAX_ROLL_ANGLE)
             {
                 m_rotation.z = Helpers.wrapAngle(
-                    m_rotation.z + (delta * INCREMENT_PITCH_SPEED));
+                    m_rotation.z + (delta * INCREMENT_TURN_SPEED));
             }
         }
 
@@ -175,7 +206,7 @@ namespace cst
             if (angle > -MAX_ROLL_ANGLE)
             {
                 m_rotation.z = Helpers.wrapAngle(
-                    m_rotation.z - (delta * INCREMENT_PITCH_SPEED));
+                    m_rotation.z - (delta * INCREMENT_TURN_SPEED));
             }
         }
 
@@ -206,16 +237,144 @@ namespace cst
         // Handles moving forward
         private void handleMoveForward(float delta)
         {
-            if (m_forwardSpeed > MAX_SPEED)
-                m_forwardSpeed = MAX_SPEED;
+            if (m_forwardSpeed > m_maxSpeed)
+                m_forwardSpeed = m_maxSpeed;
 
             if (m_forwardSpeed < 0.0f)
                 m_forwardSpeed = 0.0f;
 
-            m_position += transform.forward*m_forwardSpeed*delta;
+            m_position += m_parent.getTransform().forward 
+                * m_forwardSpeed * delta;
 
-            // Update the game object
-            transform.position = m_position;
+            m_parent.getTransform().position = m_position;
+        }
+    }
+
+    public class FlightController : IControllerType
+    {
+        private readonly SeraphController m_parent;
+
+        public FlightController(SeraphController parent)
+        {
+            m_parent = parent;
+        }
+
+        public void update()
+        {
+        }
+
+        public void triggerEnter(Collider other)
+        {
+            Debug.Log(GetType().Name + " triggerEnter()");
+        }
+
+        public void collisionEnter(Collision other)
+        {
+            Debug.Log(GetType().Name + " collisionEnter()");
+        }
+    }
+
+    public class SeraphController : MonoBehaviour
+    {
+        // Controller determines state if NONE.
+        public enum SeraphState
+        {
+            NONE,
+            GROUNDED,
+            FALLING,
+            GLIDING,
+            FLYING
+        }
+
+        public enum FlightCapability
+        {
+            NONE,
+            GLIDE,
+            FLIGHT
+        }
+
+        [SerializeField] private FlightCapability m_capability 
+            = FlightCapability.NONE;
+
+        private SeraphState m_state = SeraphState.NONE;
+
+        private GroundController m_groundController;
+        private GlideController m_glideController;
+        private FlightController m_flightController;
+        private IControllerType m_activeController;
+
+        public void Start()
+        {
+            m_groundController = new GroundController(this);
+            m_glideController = new GlideController(this);
+            m_flightController = new FlightController(this);
+        }
+
+        public void Update()
+        {
+            switch (m_state)
+            {
+                case SeraphState.NONE:
+                    // TODO Determine new state automatically.
+                    m_activeController = m_glideController;
+                    break;
+
+                case SeraphState.GLIDING:
+                    m_activeController = m_glideController;
+                    break;
+
+                case SeraphState.FLYING:
+                    m_activeController = m_flightController;
+                    break;
+
+                default:
+                    m_activeController = m_groundController;
+                    break;
+            }
+
+            m_activeController.update();
+        }
+
+        public void OnCollisionEnter(Collision other)
+        {
+            m_activeController.collisionEnter(other);
+        }
+
+        public void OnCollisionStay(Collision other)
+        {
+            // Something very bad has happened here. 
+            // TODO Teleport player to nearest safe point.
+            Debug.LogWarning("Seraph is inside an object");
+        }
+
+        public void OnTriggerEnter(Collider other)
+        {
+            m_activeController.triggerEnter(other);
+        }
+
+        public SeraphState getState()
+        {
+            return m_state;
+        }
+
+        public void setState(SeraphState state)
+        {
+            m_state = state;
+        }
+
+        public FlightCapability getCapability()
+        {
+            return m_capability;
+        }
+
+        public void setCapability(FlightCapability capability)
+        {
+            m_capability = capability;
+        }
+
+        public Transform getTransform()
+        {
+            return transform;
         }
     }
 }
