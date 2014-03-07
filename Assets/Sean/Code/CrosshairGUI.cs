56d243b950088397c7b239a957e49fd59dee9bc5
Sean Vieira <1000074@live.abertay.ac.uk>
2014-03-07 02:36:38 +0000

    Added temporary object selection method using centre of screen
    + Crappy crosshair
    + Raycasting from camera for selection

---
+++ Assets/Sean/Code/CrosshairGUI.cs
@@ -0,0 +1,36 @@
+﻿// Authored by Sean
+
+using UnityEngine;
+using System.Collections;
+
+namespace sv
+{
+    public class CrosshairGUI : MonoBehaviour
+    {
+        public Texture2D reticuleImage;
+
+        private Vector3 crosshairPos;
+        private float width, height;
+
+        // Initialise the variables;
+        void Start()
+        {
+            crosshairPos.x = 0;
+            crosshairPos.y = 0;
+            crosshairPos.z = 0;
+
+            width = reticuleImage.width / 2.0f;
+            height = reticuleImage.height / 2.0f;
+        }
+
+        // Draws the crosshair onto the screen in the centre of the screen
+        // TODO: Possibly set reticule to move with mouse/controller stick?
+        void OnGUI()
+        {
+            crosshairPos.x = (Screen.width / 2) - (reticuleImage.width / 2);
+            crosshairPos.y = (Screen.height / 2) - (reticuleImage.height / 2);
+
+            GUI.DrawTexture(new Rect(crosshairPos.x, crosshairPos.y, width, height), reticuleImage);
+        }
+    }
+}
---
+++ Assets/Sean/Code/ObjectSelection.cs
@@ -0,0 +1,58 @@
+﻿// Authored by Sean
+
+using UnityEngine;
+using System.Collections;
+
+namespace sv
+{
+    public class ObjectSelection : MonoBehaviour
+    {
+
+
+        private float rayLength = 175.0f;
+        private RaycastHit hit;
+        private Ray crosshairRay;
+
+        // Use this for initialization
+        void Start()
+        {
+            /* Empty */
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (CastRay())
+            {
+                // Currently only in for debug purposes
+                // TODO: Create a keycoded puzzle
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Ray intersects with " + hit.collider.name);
+                }
+            }
+            else
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Debug.Log("Ray has no intersections");
+                }
+            }
+        }
+
+        // Casts a ray forward from the camera
+        bool CastRay()
+        {
+            crosshairRay = new Ray(transform.position, transform.forward);
+
+            Debug.DrawRay(transform.position, transform.forward * rayLength);
+
+            if (Physics.Raycast(crosshairRay, out hit, rayLength))
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
