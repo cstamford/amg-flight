d28497c6f166e17cd9323d2e9d22efd14a117d44
cstamford <cstamford@gmail.com>
2014-03-18 21:57:06 +0000

    Added a map camera to the main scene. Added a script to disable fog for just one camera. Grouped the main scene.

---
+++ Assets/Chris/Camera Scripts/CameraDisableFog.cs
@@ -0,0 +1,27 @@
+﻿// Disabled for on pre-render, re-enabled on post-render. Used to selectively disable
+// fog on one camera.
+
+using UnityEngine;
+
+namespace cst
+{
+    public class CameraDisableFog : MonoBehaviour
+    {
+        private bool m_defaultState;
+
+        public void OnStart()
+        {
+            m_defaultState = RenderSettings.fog;
+        }
+
+        private void OnPreRender()
+        {
+            RenderSettings.fog = false;
+        }
+
+        private void OnPostRender()
+        {
+            RenderSettings.fog = m_defaultState;
+        }
+    }
+}
