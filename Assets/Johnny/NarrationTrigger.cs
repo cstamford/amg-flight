0a874d1de1b5afe1efd30ba2c6aa710e33711e52
JohnnyFlight <fire-song@hotmail.co.uk>
2014-03-20 10:37:22 +0000

    Started working on narrative triggers. Attach narrative script to an object and when player collides with it audio will play. (not yet working) Also select an object to be the barrier which will be destroyed when colliding with trigger. Need to change script so that barrier is destroyed when audio finishes playing instead.

---
+++ Assets/Johnny/NarrationTrigger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NarrationTrigger : MonoBehaviour {
+
+	public AudioClip narration;
+	public GameObject barrier;
+
+	public AudioSource audioSource;
+	private bool narrativeTriggered;
+
+	// Use this for initialization
+	void Start () {
+		narrativeTriggered = false;
+		audioSource.clip = (AudioClip)Resources.Load("footsteps-1");
+	}
+
+	void OnTriggerEnter (Collider other) {
+		print(other);
+		print(other.gameObject.tag);
+		if(narrativeTriggered == false)
+		{
+			if(other.gameObject.tag == "Player")
+			{
+				//if(audioSource.isPlaying == false)
+				//{
+					//if(narration != null) {
+					//	audioSource.Play();
+					//}
+					print("Destroying barrier");
+					if(barrier != null) {
+						Destroy(barrier);
+						print("Destroyed barrier");
+					}
+					narrativeTriggered = true;
+				//}
+			}
+		}
+	}
+}
