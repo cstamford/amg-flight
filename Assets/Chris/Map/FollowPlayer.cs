0c9ca3da27d0d8569ee253c42414b3887a670512
cstamford <cstamford@gmail.com>
2014-03-20 23:37:13 +0000

    Improved the map. Added a dynamic icon which displays the player's position on the map. Moved the map into the sky for demonstration purposes. Updated the Seraph Prefab to have both Rift and non-Rift Seraphs in one prefab - they can be toggled on and off as needs require.

---
+++ Assets/Chris/Map/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace cst
+{
+    public class FollowPlayer : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_seraph;
+
+        private void Start()
+        {
+            // If no camera has been defined in properties, try to auto-find one
+            if (m_seraph == null)
+                m_seraph = GameObject.FindWithTag("Player");
+
+            if (m_seraph == null)
+                throw new Exception("No seraph detected in the scene.");
+        }
+
+        private void Update()
+        {
+            transform.position = m_seraph.transform.position;
+        }
+    }
+}
---
+++ Assets/Chris/Map/LookAtMapCamera.cs
@@ -0,0 +1,28 @@
+﻿// Looks at the map camera
+
+using System;
+using UnityEngine;
+
+namespace cst
+{
+    public class LookAtMapCamera : MonoBehaviour
+    {
+        [SerializeField] private GameObject m_mapCamera;
+
+        void Start()
+        {
+            // If no camera has been defined in properties, try to auto-find one
+            if (m_mapCamera == null)
+                m_mapCamera = GameObject.FindWithTag("Map Camera");
+
+            if (m_mapCamera == null)
+                throw new Exception("No map camera detected in the scene.");
+        }
+
+        void Update()
+        {
+            // Basic billboarding. Not sure if this is needed.
+            transform.rotation = Quaternion.LookRotation(transform.position - m_mapCamera.transform.position);
+        }
+    }
+}
