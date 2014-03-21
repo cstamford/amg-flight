b5b4551af404051b8dbc8ae918fbebff8fae7efa
JohnnyFlight <fire-song@hotmail.co.uk>
2014-03-21 03:59:42 +0000

    Added in narrative trigger functionality. Attach script to trigger object and assign audio clip and barrier in inspector. Once trigger is hit, audio clip is played. Once audio is finished playing, barrier is destroyed.
    To-do: Add validation to make script easier to use/debug.

--- Assets/Johnny/NarrationTrigger.cs
+++ Assets/Johnny/NarrationTrigger.cs
@@ -6,34 +6,46 @@ public class NarrationTrigger : MonoBehaviour {
 	public AudioClip narration;
 	public GameObject barrier;
 
-	public AudioSource audioSource;
+	private AudioSource audioSource;
 	private bool narrativeTriggered;
 
 	// Use this for initialization
 	void Start () {
 		narrativeTriggered = false;
-		audioSource.clip = (AudioClip)Resources.Load("footsteps-1");
+		audioSource = GameObject.Find("Seraph").transform.gameObject.AddComponent<AudioSource>();
+		//audioSource = (AudioSource)Camera.main.transform.gameObject.GetComponent(typeof(AudioSource));
+		print(audioSource);
+		audioSource.clip = Resources.Load("Gliding") as AudioClip;
+	}
+	
+	void Update () {
+		//	This should only trigger if the audio has finished playing
+		if(narrativeTriggered == true)
+		{
+			if(audioSource.isPlaying == false)
+			{
+				if(barrier != null)
+				{
+					Destroy(barrier);
+					print("Destroyed barrier");
+				}
+			}
+		}
 	}
 
 	void OnTriggerEnter (Collider other) {
-		print(other);
-		print(other.gameObject.tag);
 		if(narrativeTriggered == false)
 		{
 			if(other.gameObject.tag == "Player")
 			{
-				//if(audioSource.isPlaying == false)
-				//{
-					//if(narration != null) {
-					//	audioSource.Play();
-					//}
-					print("Destroying barrier");
-					if(barrier != null) {
-						Destroy(barrier);
-						print("Destroyed barrier");
+				if(audioSource.isPlaying == false)
+				{
+					if(narration != null) {
+						audioSource.Play();
+						print("Playing narration");
 					}
 					narrativeTriggered = true;
-				//}
+				}
 			}
 		}
 	}
