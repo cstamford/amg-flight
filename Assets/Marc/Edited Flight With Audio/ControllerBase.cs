775e06ea6ee0c5d88537f2addf8cc6a1b3891b75
cstamford <cstamford@gmail.com>
2014-03-21 14:49:11 +0000

    Fix Marc's stuff.

---
+++ Assets/Marc/Edited Flight With Audio/ControllerBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using UnityEngine;
+
+namespace Marc
+{
+    public struct TransitionData
+    {
+        public float velocity { get; set; }
+        public Vector3 direction { get; set; }
+
+        public override string ToString()
+        {
+            return "Direction: " + direction + " Velocity: " + velocity;
+        }
+    }
+
+    public interface IControllerBase
+    {
+        void start(TransitionData data);
+        void update();
+        void triggerEnter(Collider other);
+        void triggerExit(Collider other);
+        void collisionEnter(Collision other);
+        void collisionExit(Collision other);
+        TransitionData transitionData();
+    }
+
+    public abstract class ControllerBase
+    {
+        public SeraphController controller
+        {
+            get { return m_controller; }
+        }
+
+        protected SeraphState state
+        {
+            get { return m_controller.state; }
+            set { m_controller.state = value; }
+        }
+
+        protected SeraphCapability capability
+        {
+            get { return m_controller.capability; }
+            set { m_controller.capability = value; }
+        }
+
+        protected Transform transform
+        {
+            get { return m_controller.transform; }
+        }
+
+        protected GameObject gameObject
+        {
+            get { return m_controller.gameObject; }
+        }
+
+        private readonly SeraphController m_controller;
+
+        protected ControllerBase(SeraphController controller)
+        {
+            if (controller == null)
+                throw new ArgumentNullException("Provided controller is null.");
+
+            m_controller = controller;
+        }
+    }
+}
---
+++ Assets/Marc/Edited Flight With Audio/FlightController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Marc
+{
+    public class FlightController : ControllerBase, IControllerBase
+    {
+        public FlightController(SeraphController controller)
+            : base(controller)
+        {}
+
+        public void start(TransitionData data)
+        {
+            Debug.Log(GetType().Name + " received transition data: " + data);
+        }
+
+        public void update()
+        {
+        }
+
+        public void triggerEnter(Collider other)
+        {
+            Debug.Log(GetType().Name + " triggerEnter()");
+        }
+
+        public void triggerExit(Collider other)
+        {
+            Debug.Log(GetType().Name + " triggerExit()");
+        }
+
+        public void collisionEnter(Collision other)
+        {
+            Debug.Log(GetType().Name + " collisionEnter()");
+        }
+
+        public void collisionExit(Collision other)
+        {
+            Debug.Log(GetType().Name + " collisionExit()");
+        }
+
+        public TransitionData transitionData()
+        {
+            return new TransitionData { direction = new Vector3() };
+        }
+    }
+}
